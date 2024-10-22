19853aa4d575c23ce1535794074345a6ca03ee00
Mili Antoni <miliantoni12@gmail.com>
2024-10-22 15:38:34 -0300

    .

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+// Room.jsx
+import React from 'react';
 
-const Room = () => {
+const Room = ({ room }) => {
   return (
-    <div>Room</div>
-  )
-}
+    <div>
+      <h3>{room.name}</h3> {/* Asegúrate de que cada objeto `room` tenga una propiedad `name` */}
+      <p>ID: {room.id}</p> {/* Puedes agregar más detalles aquí */}
+    </div>
+  );
+};
 
-export default Room
+export default Room;
