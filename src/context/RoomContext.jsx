948703d1aaec1c1dc32cecca68e55f9850776f83
Mili Antoni <miliantoni12@gmail.com>
2024-10-22 15:04:45 -0300

    estructura de las habitaciones

---
+++ src/context/RoomContext.jsx
@@ -0,0 +1,17 @@
+// RoomContext.jsx
+import React, { createContext, useState } from 'react';
+
+export const RoomContext = createContext();
+
+export const RoomProvider = ({ children }) => {
+  const [roomData, setRoomData] = useState([]);
+
+  return (
+    <RoomContext.Provider value={{ roomData, setRoomData }}>
+      {children}
+    </RoomContext.Provider>
+  );
+};
+
+
+export default RoomProvider;
